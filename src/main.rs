b667629cae7d9057e4cdec9d1cb6d1cf9140a92d
Eric Mrak <emrak@brex.com>
2023-05-03 17:16:56 -0700

    Also print UDP payloads

--- src/main.rs
+++ src/main.rs
@@ -331,7 +331,8 @@ fn process_udp(
                 destination,
                 udp_packet.get_destination(),
                 udp_packet.get_length(),
-            )
+            );
+            println!("{}", escape_payload(udp_packet.payload()))
         }
         None => println!("[{}] U Malformed packet", interface_name),
     }
